570120bc53b4186c3aff2c4a400441cbbb529fdc
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-16 15:18:33 +0700

    Membuat fungsi bubblesort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -36,3 +36,39 @@ void input()
 
 }
 
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang -1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }  
+        pass++;
+    } while (pass < nPanjang - 1);
+}
+
+void display()
+{
+    cout << "\n======================================\n";
+    cout << "   Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "======================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j <nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
+void binarySearch()
