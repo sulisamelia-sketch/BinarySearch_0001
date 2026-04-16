a3e9c83cb6c98c2f5c9b307218e8b0941a293ab9
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-16 15:23:34 +0700

    Menambahkan display function

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -54,3 +54,20 @@ void bubbleSortArray()
     } while (pass < nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n======================================\n";
+    cout << "   Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "======================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j <nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
